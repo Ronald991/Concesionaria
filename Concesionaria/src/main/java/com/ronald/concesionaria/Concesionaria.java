5504510059d873c41e15c3dcb9262e154fc2238d
Ronald <ronaldalmada91@outlook.com>
2023-08-31 22:11:21 -0400

    Clase principal de nuestra aplicación

---
+++ Concesionaria/src/main/java/com/ronald/concesionaria/Concesionaria.java
@@ -0,0 +1,18 @@
+
+package com.ronald.concesionaria;
+
+import com.ronald.concesionaria.gui.VentanaPrincipal;
+
+/**
+ *
+ * @author Ronald
+ */
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setTitle("Concesionaria");
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
